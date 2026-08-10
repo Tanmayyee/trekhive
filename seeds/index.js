80f94aa2a3ead02840fb67458a9a43a6573037c9
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-10 16:57:19 +0530

    add random location and title generation

--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import cities from "./cities";
 import { places,descriptors } from "./seedHelpers";
 
-import Trekhive from './models/trekhiveschema.js';
+import Trekhive from './models/trekhiveschema.js';  //model
 
 const MONGO_URI = "mongodb://127.0.0.1:27017/trekhive";
 
@@ -12,7 +12,25 @@ const connectDB = async () => {
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
-    process.exit(1); // Stops the server if the database fails to connect.
+    process.exit(1);
   }
 };
 await connectDB();
+
+const randomTitle= array => array[Math.floor(Math.random()*array.length)]
+
+const seedDb= async()=>{
+    await Trekhive.deleteMany({})
+    for(let i=0; i<=100; i++){
+
+        const randomCity= Math.floor(Math.random()*cities.length)
+        
+      const camp=  new Trekhive({
+            location:`${cities[randomCity].city}, ${cities[randomCity].state}`,
+            title:`${randomTitle(places)} ${randomTitle(descriptors)}`
+        })
+        await camp.save();
+    }
+}
+
+seedDb()
