169c056b873b78d5804f83012f4da67721a98329
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-10 16:30:54 +0530

    add mongoose in index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,3 +1,18 @@
 import mongoose from "mongoose";
 import cities from "./cities";
 import { places,descriptors } from "./seedHelpers";
+
+import Trekhive from './models/trekhiveschema.js';
+
+const MONGO_URI = "mongodb://127.0.0.1:27017/trekhive";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1); // Stops the server if the database fails to connect.
+  }
+};
+await connectDB();
