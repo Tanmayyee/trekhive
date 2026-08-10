da0c86d9e8fdf6841467e53d88ed3916bee99f06
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-10 09:16:06 +0530

    switch to ES modules and add mongoose

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const express = require('express');
-const path = require('path');
-
+import express from 'express';
+import path from 'path';
 const app = express();
+import mongoose from 'mongoose'
+
+const MONGO_URI = "mongodb://127.0.0.1:27017/trekhive";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1); // Stops the server if the database fails to connect.
+  }
+};
+await connectDB();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(import.meta.dirname, '/views'));
 
-// Serve the public directory for Tailwind's output
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(import.meta.dirname, "public")));
 
 app.get('/', (req, res) => {
     res.render('home');
