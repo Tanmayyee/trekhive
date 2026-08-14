7117e56b971f0cde64ce9d809db5664ba99bbe77
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-14 22:16:53 +0530

    Add global error handling for routes and database operations

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose'
 import Listing from './models/trekhiveschema.js';
 import ejsMate from 'ejs-mate'
 import methodOverride from "method-override";
-
+import ExpressError from './utils/ExpressError.js';
 
 const MONGO_URI = "mongodb://127.0.0.1:27017/trekhive";
 
@@ -52,20 +52,34 @@ app.get('/listing/new',async(req,res)=>{           //new should come before /:id
 
 app.post('/listing',async(req,res)=>{
   // res.send(req.body)                           //for testing
-
+  
+  // Throws an error if the user submits an empty form, preventing an empty database entry.
+  if(!req.body.listing){
+    throw new ExpressError('Invalid trek data',400)
+  }  
   // req.body.listing contains the listing object created from form fields named like listing[title], listing[location], etc.
-  const newListing= new Listing(req.body.listing)       
+  const newListing= new Listing(req.body.listing)     
   await newListing.save()
   res.redirect(`/listing/${newListing._id}`)
 })
 
 app.get('/listing/:id',async(req,res)=>{
   const listing= await Listing.findById(req.params.id)
+  
+  // Throws error if someone types a URL for a trek that doesn't exist.
+  if(!listing){
+    throw new ExpressError('Trek not found',404)
+  }
   res.render('places/show',{listing})
 })
 
 app.get('/listing/:id/edit',async(req,res)=>{
   const listing= await Listing.findById(req.params.id)
+  
+  // Throws an error so the app doesn't try to load an edit form for a trek that has already been deleted.
+  if(!listing){
+    throw new ExpressError('Trek not found',404)
+  }
   res.render('places/edit',{listing})
 })
 
@@ -73,15 +87,36 @@ app.put('/listing/:id',async(req,res)=>{
   // res.send("workeddd")
   const {id}= req.params
   const updatedListing= await Listing.findByIdAndUpdate(id,{...req.body.listing},{ runValidators: true, returnDocument: "after" })  // Spread operator (...) creates a new object containing all properties from req.body.listing. This lets us pass the listing fields directly to findByIdAndUpdate().
+  
+   // Throws a 404 error just in case the trek was deleted by someone else exactly when you clicked "update".
+   if (!updatedListing) {
+        throw new ExpressError('Trek not found', 404);
+    }
   res.redirect(`/listing/${updatedListing._id}`)  
 })
 
 app.delete('/listing/:id',async(req,res)=>{
   const {id}=req.params;
-  await Listing.findByIdAndDelete(id)
+  const deleted= await Listing.findByIdAndDelete(id)
+  
+  // Throws a 404 error to let the app know it can't delete something that is already missing from the database.
+  if(!deleted){
+    throw new ExpressError('Trek not found',404)
+  }
   res.redirect('/listing')
 })
 
+//for paths/routes other than previosly defined(above) paths
+app.all('/{*path}',(req,res)=>{
+  throw new ExpressError('Page not found',404)
+})
+
+
+//error handling middleware or global error
+app.use((err,req,res,next)=>{
+  const {message='Something went wrong' , status=500}= err;
+  res.status(status).send(message);
+})
 
 app.listen(3000, () => {
     console.log('Trekhive server is running on port 3000!');
