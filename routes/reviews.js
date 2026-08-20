4e73fac9fee47dd3b9d4e33904b9cbf1c67749f7
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-20 17:16:46 +0530

    refactor: move review routes to router

---
+++ routes/reviews.js
@@ -0,0 +1,41 @@
+import express from 'express'
+const router=express.Router({mergeParams:true});   // to use params , or id in this case
+import Listing from '../models/trekhiveschema.js';
+import Review from '../models/reviewmodel.js';
+import ExpressError from '../utils/ExpressError.js';
+import { reviewValidationSchema } from '../utils/validationSchema.js';
+
+const reviewValidation=(req,res,next)=>{
+  const {error}=reviewValidationSchema.validate(req.body,{abortEarly:false})
+  if(error){
+    const err=error.details.map(el=>el.message.replace(/"/g, '').replace(/review\./g, '')).join(' | ')
+    throw new ExpressError(err,400)
+  }else{
+    next()
+  }
+}
+
+// review submission route
+router.post('/',reviewValidation,async(req,res)=>{
+     const foundListing= await Listing.findById(req.params.id)
+     if(!foundListing){
+      throw new ExpressError('Trek not found',404)
+     }
+     const newReview= new Review(req.body.review)      //inside form - review[body] , review[rating]
+     foundListing.reviews.push(newReview);
+     await newReview.save()
+     await foundListing.save()
+    //  res.send(foundListing) //to check 
+    res.redirect(`/listing/${foundListing._id}`)
+})
+
+router.delete('/:reviewId',async(req,res)=>{
+  const {id,reviewId}= req.params
+  await Listing.findByIdAndUpdate(id,{$pull: {reviews:reviewId}})   //mongo - pull operator - removes elements from an array that match a specified condition.
+  await Review.findByIdAndDelete(reviewId)
+  res.redirect(`/listing/${id}`)
+  // res.send('working ')
+})
+
+export default router;
+
