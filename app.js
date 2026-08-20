16d2b301e48eab4cca19bbfb0183222435c32495
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-20 17:16:46 +0530

    refactor: move review routes to router

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ import express from 'express';
 import path from 'path';
 const app = express();
 import mongoose from 'mongoose'
-import Review from './models/reviewmodel.js';
 import ejsMate from 'ejs-mate'
 import methodOverride from "method-override";
 import ExpressError from './utils/ExpressError.js';
-import { reviewValidationSchema } from './utils/validationSchema.js';
 import listings from './routes/listings.js'
+import reviews from './routes/reviews.js'
 
 const MONGO_URI = "mongodb://127.0.0.1:27017/trekhive";
 
@@ -34,49 +33,12 @@ app.use(methodOverride("_method"));
 
 app.use('/listing',listings);
 
+app.use('/listing/:id/reviews',reviews)
+
 app.get('/', (req, res) => {
     res.render('places/home');
 });
 
-// =========================== to check model working or not =============================
-// app.get('/trekhivedb',async(req,res)=>{
-//   const check =new Listing({title:"hello checking",price:23})
-//   await check.save()
-//   res.send(check)
-// })
-
-const reviewValidation=(req,res,next)=>{
-  const {error}=reviewValidationSchema.validate(req.body,{abortEarly:false})
-  if(error){
-    const err=error.details.map(el=>el.message.replace(/"/g, '').replace(/review\./g, '')).join(' | ')
-    throw new ExpressError(err,400)
-  }else{
-    next()
-  }
-}
-
-
-// review submission route
-app.post('/listing/:id/reviews',reviewValidation,async(req,res)=>{
-     const foundListing= await Listing.findById(req.params.id)
-     if(!foundListing){
-      throw new ExpressError('Trek not found',404)
-     }
-     const newReview= new Review(req.body.review)      //inside form - review[body] , review[rating]
-     foundListing.reviews.push(newReview);
-     await newReview.save()
-     await foundListing.save()
-    //  res.send(foundListing) //to check 
-    res.redirect(`/listing/${foundListing._id}`)
-})
-
-app.delete('/listing/:id/reviews/:reviewId',async(req,res)=>{
-  const {id,reviewId}= req.params
-  await Listing.findByIdAndUpdate(id,{$pull: {reviews:reviewId}})   //mongo - pull operator - removes elements from an array that match a specified condition.
-  await Review.findByIdAndDelete(reviewId)
-  res.redirect(`/listing/${id}`)
-  // res.send('working ')
-})
 
 //for paths/routes other than previosly defined(above) paths
 app.all('/{*path}',(req,res)=>{
