6042133fe565cfd0ab8323a4a19c14d8d669fbda
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-22 17:28:27 +0530

    feat: add registration form and post route with error handling

--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,20 @@ router.get('/register',(req,res)=>{
     res.render('auth/register')
 })
 
+router.post('/register',async(req,res)=>{
+    // res.send(req.body)
+    try{
+         const {username,password,email}= req.body
+         const user=new User({email,username})
+         const registeredUser= await User.register(user,password)
+         // console.log(registeredUser)
+         req.flash('success','welcome!')
+         res.redirect('/listing')
+    }catch(e){
+        req.flash('error',e.message)
+        res.redirect('/register')
+    }
+   
+})
+
 export default router
