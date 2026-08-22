09801e6d7021dca3b32eef3adb2032878f67c629
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-22 19:29:59 +0530

    feat: add isLoggedIn middleware

---
+++ middleware.js
@@ -0,0 +1,7 @@
+export const isLoggedIn=(req,res,next)=>{
+    if(!req.isAuthenticated()){
+        req.flash('error','You must be signed in first!')
+        return res.redirect('/login')
+    }
+    next()
+}
