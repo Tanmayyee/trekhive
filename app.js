1971ca75680aa58fc94f7a53a2781980ed804b6b
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-22 16:12:29 +0530

    feat: configuring passport

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ import listings from './routes/listings.js'
 import reviews from './routes/reviews.js'
 import session, { Cookie } from 'express-session';
 import flash from 'connect-flash'
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import User from './models/usermodel.js'
 
 
 const MONGO_URI = "mongodb://127.0.0.1:27017/trekhive";
@@ -48,12 +51,27 @@ const sessionConfig= {
 app.use(session(sessionConfig))
 
 app.use(flash())
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 app.use((req,res,next)=>{
   res.locals.success= req.flash('success');
   res.locals.error= req.flash('error')
   next();
 })
 
+// to check working of passport->
+
+app.get('/fakeuser',async(req,res)=>{
+  const u= new User({email:'samplee@gmail.com',username:'tanmayy'})
+  const newUser= await User.register(u,'mypassword')   //method provide by passport 
+  res.send(newUser)
+})
 
 app.use('/listing',listings);
 
