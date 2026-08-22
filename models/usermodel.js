4f9012de5c00b8ddf05add3a28027491fd84944b
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-22 15:41:26 +0530

    feat: add usermodel with passport-local-mongoose

---
+++ models/usermodel.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+import passportLocalMongoose from 'passport-local-mongoose'
+
+const userSchema= new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    }
+})
+
+// Adds authentication features like username, password hashing, salt, and login methods to the schema
+userSchema.plugin(passportLocalMongoose.default);   
+
+export default userSchema;
