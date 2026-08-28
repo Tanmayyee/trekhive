8faeb67b57f96e89a00425b6e878cda6782bdc70
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-28 23:19:22 +0530

    update DB_URL

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ import MongoStore from 'connect-mongo';
 
 const dbUrl= process.env.DB_URL
 // const dbUrl= "mongodb://127.0.0.1:27017/trekhive-v2"
-//updated DB_URL
+//updated DB_URL env
 
 const MONGO_URI = dbUrl || "mongodb://127.0.0.1:27017/trekhive-v2" ;
 
